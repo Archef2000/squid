9d9d598aad181c1c4d06cbd13bfcb74d7254cd45
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-04-10 22:24:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ then
   echo "http_port 3128
   http_access allow all" > /etc/squid/squid.conf
 else
-    if [ ! -z $USER_1 ]
+  
+  if [ ! -z $USER_1 ]
   then
     echo "${USER_1:=user1}":$(openssl passwd -apr1 ${PASS_1:=passwd123}) > /etc/squid/passwords
   fi
@@ -26,7 +27,16 @@ else
   then
     echo "$USER_1":$(openssl passwd -apr1 $PASS_1) > /etc/squid/passwords
   fi
-  
+
+  if [ ! -z $USER_1 ]
+  then
+    echo "Default User is "user1""
+  fi
+  if [ ! -z $PASS_1 ]
+  then
+    echo "Default Password is "passwd123""
+  fi
+
   echo "auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwords
   auth_param basic realm proxy
   acl authenticated proxy_auth REQUIRED
